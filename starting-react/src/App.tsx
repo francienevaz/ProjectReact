7c334c74507a7f01933abcbc9ffdd7c7990f94e3
francienevaz <fran.vfraga@gmail.com>
2022-03-11 12:35:48 -0300

    updated

--- starting-react/src/App.tsx
+++ starting-react/src/App.tsx
@@ -1,7 +1,29 @@
+import { useState } from "react"
 import {Tweet} from "./components/Tweet"
+import './App.css'
 
 function App() {
-  return <Tweet/>
+  // Basicamente o UseState cria uma variável que é monitorada pelo componente, e toda vez que a variável muda esse valor o componente é remontado/rerenderizado em tela com as novas informações daquela variável.
+  const [tweets, setTweets] = useState<string[]>([
+    "Tweet 1",
+    "Tweet 2",
+    "Tweet 3",
+    "Tweet 4"
+  ])
+
+  function createTweet() {
+    setTweets([...tweets, "Tweet 5"])
+  }
+
+  return (
+    <>
+    {/* Aqui chamei a constante tweet e usei o método Map para percorrer dentro da array  */}
+    {tweets.map(tweet => {
+      return <Tweet text={tweet}/>
+    })}
+    <button onClick={createTweet}>Adicionar Tweet</button>
+      </>    
+  )
 }
 
 export default App
